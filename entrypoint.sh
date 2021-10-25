cf20e729007a0dfb7afcdc1cc8a7321eef940f5c
thatcooldevguy <tsommers150@gmail.com>
2021-10-25 18:26:21 -0400

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 echo "WARNING: Please ensure your bot files are in the root of container. Ensure a package.json file is present, or upload one."
-npm i -g 
+echo "WARNING: Please ensure your main botfile is named index.js"
+echo "setting up NPM"
+cd /home/container
+echo "Running npm i -g...."
+npm install
+echo "running node ."
 node . 
